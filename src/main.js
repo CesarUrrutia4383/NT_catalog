a8db6986e24e57c9872646703e2ad38bb6a18ee4
I_4m_SilverGWolf <ssoto4383@gmail.com>
2025-07-14 11:39:17 -0600

    Adicion de funciones fluidas y ademas pagina de contacto empresarial junto a ubicacion y correcciones menores a contenidos mostrados

---
+++ src/main.js
@@ -0,0 +1,25 @@
+//CSS DE ESTILOS PARA LAS SECCIONES DE LAS PAGINAS
+import "./css/normalize.css";
+import "./css/variables.css";
+import "./css/estilos.css";
+import "./css/header.css";
+import "./css/hero.css";
+import "./css/caracteristicas.css";
+import "./css/footer.css";
+import "./css/marcas.css";
+import "./css/contacto.css";
+
+//FUNCIONES DE JAVASCRIPT
+import "./js/video.js";
+import "./js/slideshow.js";
+import "./js/caracteristicas.js";
+import "./js/aos_init.js"
+
+//AOS PARA ANIMACIONES
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1000,
+  once: false
+});
