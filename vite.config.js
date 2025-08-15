a976d8364dbba833a1e1cad6762bdc8efca91c0e
I_4m_SilverGWolf <ssoto4383@gmail.com>
2025-08-15 09:27:36 -0600

    Modificacion para desplegar las paginas a produccion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     allowedHosts: ['nt-catalog.onrender.com'],
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        catalog: resolve(__dirname, 'catalog.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
 });
